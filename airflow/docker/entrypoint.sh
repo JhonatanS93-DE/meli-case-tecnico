6c48dcb1c4eeaa93af2141116489781b8ed6ce23
Jhonatan Saldarriaga <jhonatan1393@gmail.com>
2025-06-22 11:08:21 -0500

    Se corrigen error del entrypoint para usuario y contraseña de Airflow

--- airflow/docker/entrypoint.sh
+++ airflow/docker/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-airflow db init
-airflow users create \
-    --username admin \
-    --firstname Admin \
-    --lastname User \
-    --role Admin \
-    --email admin@example.com \
-    --password admin
-exec airflow webserver
 
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  airflow db init
+fi
+
+airflow users list | grep -q 'admin' || \
+airflow users create \
+  --username admin \
+  --firstname Admin \
+  --lastname User \
+  --role Admin \
+  --email admin@example.com \
+  --password admin
+
+exec "$@"

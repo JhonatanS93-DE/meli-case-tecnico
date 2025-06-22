912221f7a62c40fd34e99ca7246929f06edb3709
Jhonatan Saldarriaga <jhonatan1393@gmail.com>
2025-06-22 10:34:42 -0500

    Se corrigen errores del docker

---
+++ airflow/docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+airflow db init
+airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
+exec airflow webserver
+
